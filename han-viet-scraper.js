0d895bcf71e65d32fa55945b25859d33701497ba
Thach Tran <rizzaxc@gmail.com>
2020-03-27 22:05:12 +1100

    first commit

---
+++ han-viet-scraper.js
@@ -0,0 +1,28 @@
+const axios = require('axios').default
+const cheerio = require('cheerio')
+
+const thiVienUrl = "https://hvdic.thivien.net/whv/"
+
+
+module.exports = class HanVietScraper {
+    async lookup(kanji) {
+        let kanjiUrl = encodeURI(thiVienUrl + kanji)
+        let amHanViet = new Set()
+        // let nghiaPhoThong = new Set()
+
+        await axios.get(kanjiUrl)
+            .then(response => {
+                const $ = cheerio.load(response.data)
+                $('.info .hvres-goto-link').each((index, element) => {
+                    amHanViet.add($(element).text())
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+
+        return Array.from(amHanViet).join(' ')
+    }
+
+}
+
---
+++ index.js
@@ -0,0 +1,18 @@
+const JishoApi = require('unofficial-jisho-api');
+const jisho = new JishoApi();
+let HanVietScraper = require('./han-viet-scraper')
+let scraper = new HanVietScraper()
+
+
+kanji = '安'
+
+console.log(kanji)
+
+scraper.lookup(kanji).then(amHanViet => {
+  console.log('Am Han Viet: ' + amHanViet)
+})
+
+jisho.searchForKanji(kanji).then(result => {
+  console.log('Meaning: ' + result.meaning)
+})
+
